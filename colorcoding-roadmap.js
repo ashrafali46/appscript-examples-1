8738c0ebf83ef66c00ab9bc49a136098883b5a6e
Christine Hall <christinehall@users.noreply.github.com>
2016-04-05 08:36:41 -0700

    Added comments and TODOs

--- colorcoding-roadmap.js
+++ colorcoding-roadmap.js
@@ -1,3 +1,10 @@
+// if you set this trigger to fire on edit, it will:
+// 1. Listen for updates in your spreadsheet.
+// 2. If any of those updates take place on a certain sheet...
+// 3. ...and in a specific column of that sheet...
+// 4. ...it will color the entire row based on the input of that cell's string value.
+// Make sure to update everything marked with a TODO below 
+
 function onEdit(e) {
     if (e) {
         var ss = e.source.getActiveSheet();
@@ -7,13 +14,14 @@ function onEdit(e) {
         // do not work in first row and only work in the sheet called "Roadmap"
         if (r.getRow() != 1 && ss.getName() == "Roadmap") {
 
-          // E.g. status column is 2nd (B)
-          status = ss.getRange(r.getRow(), 1).getValue();
+          var updateCol = 1 // TODO: update this to whatever column you are basing the color of the row from.
+          status = ss.getRange(r.getRow(), updateCol).getValue();
 
           // Specify the range with which You want to highlight
-          var numCols = 9 // update this with how many columns your spreadsheet has
+          var numCols = 9 // TODO: update this with how many columns your spreadsheet has
           rowRange = ss.getRange(r.getRow(),1,1,numCols);
 
+          // TODO: update the colors you'd like to use in your sheet. Use hex.
           var grey = "#E0E0E0";
           var lightGreen = "#B8E986";
           var darkGreen = "#7ED321";
@@ -21,6 +29,7 @@ function onEdit(e) {
           var lightRed = "#FFD0D6"
 
           // This changes font color
+          // TODO: update each status == "something" check below to match whatever strings you expect in the column you are watching. 
           if (status == 'Testing') {
             rowRange.setBackground(grey);
 
